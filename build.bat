b7b3e351b0c5ea8cfc8d1dae19678a4caf413505
JamesPCVR <jamespazcundy@gmail.com>
2024-12-29 01:33:33 +0000

    update builder

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
-ECHO "Building MusiGui from source wuth pyinstaller"
+@ECHO OFF
+
+ECHO "Building MusiGui from source with nuitka"
 
 nuitka src\main.py --standalone --enable-plugin=pyside6 --nofollow-import-to=yt_dlp.extractor.lazy_extractors --output-filename=MusiGui.exe --output-dir=compile --windows-console-mode=disable --windows-icon-from-ico=src\assets\musigui.ico
 
@@ -8,12 +10,14 @@ xcopy "src\assets" "compile\main.dist\assets" /s /i
 
 ECHO "Rename main.dist to musigui"
 
-ren "compile\main.dist" "compile\musigui"
+cd compile
+ren "main.dist" "musigui"
 
 ECHO "Compressing build"
 
-tar.exe -a -cf compile/musigui-v0.0.0.zip compile/musigui/*
+tar.exe -a -cf musigui-v0.0.0.zip musigui/*
 
 ECHO "Build zipped"
 
-pause
+PAUSE
+cmd /k
